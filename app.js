d57d5f38d3bd6a94be20d3fc6a83863d01487f31
Juan Alan <juan01alan@gmail.com>
2022-09-19 13:01:01 -0300

    first

---
+++ app.js
@@ -0,0 +1,16 @@
+﻿const navbtn = document.querySelector('.navbtn');
+const navbar = document.getElementById('navbar');
+const itemsnavbar = document.querySelectorAll('.navbar ul li');
+
+navbtn.addEventListener('click',()=>{
+    if (navbar.style.left == '-110%') {
+        navbar.style.left = '0%';
+    } else {   
+        navbar.style.left = '-110%';
+    }
+});
+
+itemsnavbar.forEach((item)=>{
+    item.addEventListener('click',()=>{
+        navbar.style.left = '-110%';
+})});
